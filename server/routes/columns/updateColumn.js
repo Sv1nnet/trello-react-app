c4006210ae30ab96de9e95f9ed38ce80088a4d6f
Leonid Tsukanov <39421667+Sv1nnet@users.noreply.github.com>
2020-01-12 07:21:36 +0300

    addActivity added into updateColumn (for title update)

--- server/routes/columns/updateColumn.js
+++ server/routes/columns/updateColumn.js
@@ -1,11 +1,15 @@
 const jwt = require('jsonwebtoken');
 const { Board } = require('../../models/Board');
+const { Column } = require('../../models/Column');
+
+const addActivity = require('../../utils/addActivity');
 
 const updateColumn = (req, res) => {
   const token = req.headers.authorization.split(' ')[1];
   const boardId = req.params.id;
   const { columnId } = req.params;
   const { dataToUpdate } = req.body;
+  console.log(req.body)
 
   jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
     if (err) {
@@ -23,6 +27,7 @@ const updateColumn = (req, res) => {
       const isMember = isOwner || board.members.find(member => member._id.toHexString() === decoded._id);
 
       if (isOwner || (isMember && !board.isReadOnly)) {
+        const columnToUpdate = await Column.findById(columnId);
         board.updateColumn(columnId, dataToUpdate);
 
         const savedBoard = await board.save().catch((err) => {
@@ -30,7 +35,24 @@ const updateColumn = (req, res) => {
           return Promise.reject(new Error('Could not save the board with a new column'));
         });
 
-        return res.status(200).send(savedBoard);
+        const propToUpdate = Object.keys(dataToUpdate)[0];
+        const { activity, updatedBoard, error } = await addActivity(
+          'column',
+          {
+            type: 'rename',
+            data: {
+              authorId: decoded._id,
+              sourceId: columnId,
+              boardId: savedBoard._id.toHexString(),
+              date: new Date().toString(),
+              prevName: columnToUpdate.title,
+              newName: dataToUpdate[propToUpdate],
+            },
+          },
+        );
+
+        const activities = await updatedBoard.getActivities();
+        return res.status(200).send({ ...updatedBoard._doc, activities });
       }
 
       res.status(400).send({ err: 'Only board owner can remove new columns' });
