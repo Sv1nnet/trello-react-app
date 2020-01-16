da67557535578df30ed55cafa94def856be51067
Leonid Tsukanov <39421667+Sv1nnet@users.noreply.github.com>
2020-01-16 16:55:08 +0300

    addActivity when a card moves to another column

--- server/routes/cards/updateCardPositions.js
+++ server/routes/cards/updateCardPositions.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 
 const { Board } = require('../../models/Board');
 
+const addActivity = require('../../utils/addActivity');
+
 const updateCardPositions = (req, res) => {
   const token = req.headers.authorization.split(' ')[1];
   const boardId = req.params.id;
@@ -14,6 +16,7 @@ const updateCardPositions = (req, res) => {
     }
 
     try {
+      let activityData;
       const board = await Board.findById(boardId)
         .catch((err) => {
           console.log('Could not find a board', err);
@@ -24,19 +27,52 @@ const updateCardPositions = (req, res) => {
       const isMember = isOwner || board.members.find(member => member._id.toHexString() === decoded._id);
 
       if (isOwner || (isMember && !board.isReadOnly)) {
-        cards.forEach((card) => {
+        cards.forEach(async (card) => {
           const dataToUpdate = {
             position: card.position,
             column: card.column,
           };
 
+          const currentCard = board.cards.find(cardOnBoard => cardOnBoard._id.toHexString() === card._id);
+
+          if (currentCard.column.toHexString() !== card.column) {
+            const prevColumn = board.columns.find(column => column._id.toHexString() === currentCard.column.toHexString());
+            const newColumn = board.columns.find(column => column._id.toHexString() === card.column);
+
+            activityData = [
+              'card',
+              {
+                type: 'moved',
+                data: {
+                  boardId,
+                  sourceId: card._id,
+                  authorId: decoded._id,
+                  date: new Date(),
+                  cardName: card.title,
+                  prevName: prevColumn.title,
+                  newName: newColumn.title,
+                },
+              },
+            ];
+          }
+
           board.updateCard(card._id, dataToUpdate);
         });
 
-        const savedBoard = await board.save().catch((err) => {
+        let savedBoard = await board.save().catch((err) => {
           console.log('Could not save board with a new card positions', err);
           return Promise.reject(new Error('Could not save the board with a new card positions'));
         });
+
+        if (activityData) {
+          const result = await addActivity(
+            activityData[0],
+            activityData[1],
+          );
+
+          // eslint-disable-next-line prefer-destructuring
+          savedBoard = result.updatedBoard;
+        }
 
         const activities = await savedBoard.getActivities();
 
