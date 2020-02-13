75fce3e8497417ee81a56cbe5bec989cf96b6515
Leonid Tsukanov <39421667+Sv1nnet@users.noreply.github.com>
2020-02-13 19:02:11 +0300

    useStatus functions description added

--- src/utlis/hooks/useStatus.js
+++ src/utlis/hooks/useStatus.js
@@ -16,10 +16,23 @@ const defaultStatus = {
 const useStatus = (initialStatus = defaultStatus) => {
   const [status, setStatus] = useState(initialStatus);
 
+  /**
+   * Set status.lading as true.
+   */
   const setStatusLoading = () => {
     setStatus(prevStatus => ({ ...prevStatus, loading: true }));
   };
 
+  /**
+   * Handle success response from a server. It sets status.loading as false,
+   * status.err.statusCode and status.err.message as null.
+   * status.success changes listied in types.
+   * @param {Object} res response object
+   * @param {number} res.status response object
+   * @param {string} res.message response object
+   * @param {any} res.data response object
+   * @return res argument.
+   */
   const handleSuccess = (res) => {
     setStatus({
       loading: false,
@@ -37,6 +50,15 @@ const useStatus = (initialStatus = defaultStatus) => {
     return res;
   };
 
+  /**
+   * Handle error response from a server. It sets status.loading as false,
+   * status.success.statusCode, status.success.message and status.success.data as null.
+   * status.err changes listied in types.
+   * @param {Object} err response object
+   * @param {number} err.status response object
+   * @param {string} err.message response object
+   * @return Promise.reject(err).
+   */
   const handleError = (err) => {
     setStatus({
       loading: false,
@@ -54,6 +76,9 @@ const useStatus = (initialStatus = defaultStatus) => {
     return Promise.reject(err);
   };
 
+  /**
+   * Set all status props as null and loading as false.
+   */
   const resetStatus = () => {
     setStatus({
       loading: false,
