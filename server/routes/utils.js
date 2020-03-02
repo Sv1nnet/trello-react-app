fdd96b97291bcec5e237917c3cc6adeb636eab94
Leonid Tsukanov <39421667+Sv1nnet@users.noreply.github.com>
2020-03-02 22:11:26 +0300

    findCardWorker module now is findCard.worker

--- server/routes/utils.js
+++ server/routes/utils.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
-const findCardWorker = require('./utils/findCardWorker');
+const findCardWorker = require('./utils/findCard.worker.js');
 
 router.get('/findCardWorker.js', findCardWorker);
 
